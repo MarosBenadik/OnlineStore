3a7e84fcd0b8558276e51a789b00dac515a2cfd4
MarosBenadik <94084260+MarosBenadik@users.noreply.github.com>
2024-09-08 10:53:13 +0200

    Update base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -101,8 +101,8 @@ BASKET_SESSION_ID = "basket"
 
 # Custom user model
 AUTH_USER_MODEL = "account.Customer"
-LOGIN_REDIRECT_URL = "/django/account/dashboard"
-LOGIN_URL = "/django/account/login/"
+LOGIN_REDIRECT_URL = "/account/dashboard"
+LOGIN_URL = "/account/login/"
 
 # Email setting
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
